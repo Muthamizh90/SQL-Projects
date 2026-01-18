2bdb9a324587fe52050ba53bdd2f15470e8e152b
Muthamizh90 <zenthamizh@gmail.com>
2026-01-18 20:48:02 +0400

    Update Dynamic Unpivot.sql

--- Dynamic Unpivot.sql
+++ Dynamic Unpivot.sql
@@ -1,19 +1,25 @@
-use test;
+-- Dataset file: pivoted_sales_dataset.csv
 
 select * from pivoted_sales_dataset;
 
+
 -- DYNAMIC UNPIVOT
+
+
+--Declare variables for Dynamic columns and Query
 
 DECLARE @unpivotcols NVARCHAR(MAX), @unpivotquery NVARCHAR(MAX);
 
+-- Get Column names
 SELECT @unpivotcols = 
 					STRING_AGG(QUOTENAME(name),',')
 					FROM sys.columns WHERE object_id = OBJECT_ID('pivoted_sales_dataset') AND name NOT IN ('City','Product');
-
+-- Query
 SET @unpivotquery = 
 				'SELECT City, Product, SalesPerson, SaleAmount
 				FROM pivoted_sales_dataset
 				UNPIVOT(
 				SaleAmount FOR SalesPerson IN (' +@unpivotcols+ ')) AS UPVT';
 
+-- Execute Query
 EXEC sp_executesql @unpivotquery;
