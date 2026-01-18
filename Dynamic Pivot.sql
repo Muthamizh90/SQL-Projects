8907a5234d6cd154ef2775ec374ce6abadf46c33
Muthamizh90 <zenthamizh@gmail.com>
2026-01-18 20:50:51 +0400

    Update Dynamic Pivot.sql

--- Dynamic Pivot.sql
+++ Dynamic Pivot.sql
@@ -1,29 +1,26 @@
---Use database
-use test;
+-- Dataset File: sales_dataset.csv
 
-
-
+-- View Dataset
 select * from sales_dataset;
 
 -- DYNAMIC PIVOT
--- Get Product category sold in each region
-SELECT DISTINCT Category
-FROM sales_dataset;
+-- To Get Total Sales by Product category in each region
 
-SELECT DISTINCT City
-FROM sales_dataset;
-
+-- Declare variable for columns and query
 DECLARE @pivotcols NVARCHAR(MAX), @pivotquery NVARCHAR(MAX);
 
+-- Get column names dynamically
 SELECT @pivotcols = 
 				STRING_AGG(QUOTENAME(Category),',')
 				FROM (SELECT DISTINCT Category FROM sales_dataset) AS PC;
-
+-- Query
 SET @pivotquery = 
 				'SELECT *
 				FROM (SELECT City, Category, TotalAmount FROM sales_dataset) AS PQS
 				PIVOT(
 				SUM(TotalAmount) FOR Category IN (' + @pivotcols + ')) AS PVT';
 
+-- Execute Query
 EXEC sp_executesql @pivotquery;
+
 
